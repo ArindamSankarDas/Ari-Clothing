d0fea10b1392749f8bf4ef8c287775788ef0bb1b
Arindam Sankar Das <arindamsankar@outlook.com>
2021-08-02 08:20:24 +0530

    broken the homepage into samller components and finished styling the homepage menu-items

---
+++ src/Pages/homepage/HomePage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuList from "../../Components/menu-list/menu-list.component";
+import "./HomePage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <MenuList />
+    </div>
+  );
+};
+
+export default HomePage;
