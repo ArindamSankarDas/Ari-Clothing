f5c847cc49514224c75dda6a53b51824ea13aa69
Arindam Sankar Das <arindamsankar@outlook.com>
2021-08-03 20:49:00 +0530

    adjusted the menu-item to have navigation functionality with the help of react-router

--- src/Components/menu-list/menu-list.component.jsx
+++ src/Components/menu-list/menu-list.component.jsx
@@ -11,33 +11,28 @@ class MenuList extends React.Component {
         {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/hats",
+          linkUrl: "hats",
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
           linkUrl: "shop/jackets",
         },
         {
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
           linkUrl: "shop/sneakers",
         },
         {
           title: "womens",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
-          id: 4,
           linkUrl: "shop/womens",
         },
         {
           title: "mens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
-          id: 5,
           linkUrl: "shop/mens",
         },
       ],
@@ -46,10 +41,8 @@ class MenuList extends React.Component {
   render() {
     return (
       <div className="menu-list">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => {
-          return (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-          );
+        {this.state.sections.map(({ ...otherSectionProps }, index) => {
+          return <MenuItem key={index} {...otherSectionProps} />;
         })}
       </div>
     );
