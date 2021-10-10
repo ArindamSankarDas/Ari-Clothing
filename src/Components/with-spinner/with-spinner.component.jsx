f284b564c6f4ef64992513e777ffb308ac4de4d2
Arindam Sankar Das <arindamsankar@outlook.com>
2021-10-10 21:09:04 +0530

    finally fixed the error

--- src/Components/with-spinner/with-spinner.component.jsx
+++ src/Components/with-spinner/with-spinner.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 
-const WithSpinner = (WrappedComponent) => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    isLoading ? (
+const WithSpinner =
+  (WrappedComponent) =>
+  ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
       <SpinnerOverlay>
         <SpinnerContainer />
       </SpinnerOverlay>
@@ -11,7 +12,5 @@ const WithSpinner = (WrappedComponent) => {
       <WrappedComponent {...otherProps} />
     );
   };
-  return Spinner;
-};
 
 export default WithSpinner;
